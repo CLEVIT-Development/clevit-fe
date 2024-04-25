2fc864bfbb8534a0ae96dbf05dd9b90f6fb0f654
hovo-dev <hovhannes.baghdasaryan.03@gmail.com>
2024-04-25 19:15:32 +0400

    feat: add header layout fix

--- src/common/constants/projects.constants.ts
+++ src/common/constants/projects.constants.ts
@@ -1,5 +1,9 @@
+import FlowOpsMobile from "@/assets/images/projects/FlowOpsMobile.png";
+import FlowopsDesktop from "@/assets/images/projects/FlowopsDesktop.png";
 import OblivioniDesktop from "@/assets/images/projects/OblivioniDesktop.png";
 import OblivioniMobile from "@/assets/images/projects/OblivioniMobile.png";
+import RenegadeDesktop from "@/assets/images/projects/RenegadeDesktop.png";
+import RenegadeMobile from "@/assets/images/projects/RenegadeMobile.png";
 
 export const projectsConstants = [
   {
@@ -11,21 +15,34 @@ export const projectsConstants = [
       "Specializing in high-end motion pictures and commercial advertising, Oblivioni connects composers with those needing custom music. We've built an automated system and comprehensive admin section for efficient order management.",
     type: "Web Development",
     duration: "1 year",
-    link: "https://oblaivioni.com/",
+    link: "https://oblivioni.com/",
     desktopBackground: OblivioniDesktop,
     mobileBackground: OblivioniMobile,
   },
   {
     id: 2,
-    sphera: "Musician",
-    title: "Oblivioni",
-    subTitle: "The creative powerhouse that propels your visuals to new heights.",
+    sphera: "Events & Entertainment",
+    title: "TheFlowOps",
+    subTitle: "The ultimate booking platform designed to streamline your planning.",
     description:
-      "Specializing in high-end motion pictures and commercial advertising, Oblivioni connects composers with those needing custom music. We've built an automated system and comprehensive admin section for efficient order management.",
+      "TheFlowOps is a streamlined booking app for seat reservations and event planning. Our expertise in app development ensures automated systems and intuitive features for both in-person and online events. Partnered with venues, it offers a wide selection of spaces, while continuous improvements keep it competitive.",
+    type: " App Development",
+    duration: "2 years",
+    link: "https://www.theflowops.com/ ",
+    desktopBackground: FlowopsDesktop,
+    mobileBackground: FlowOpsMobile,
+  },
+  {
+    id: 3,
+    sphera: "IT Services and IT Consulting",
+    title: "Renegade Technology",
+    subTitle: "The innovative full-service IT Managed Service Provider.",
+    description:
+      "Renegade values genuine client relationships built on transparency. We developed their website emphasizing the importance of educating clients about evolving digital landscapes. ",
     type: "Web Development",
     duration: "1 year",
-    link: "https://oblivioni.com/",
-    desktopBackground: OblivioniDesktop,
-    mobileBackground: OblivioniMobile,
+    link: "https://renegadetechnology.com/ ",
+    desktopBackground: RenegadeDesktop,
+    mobileBackground: RenegadeMobile,
   },
 ];
--- src/common/layout/Layout.tsx
+++ src/common/layout/Layout.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 import Footer from "@/common/layout/Footer.tsx";
-import Heading from "@/common/layout/Heading.tsx";
+
+import HeadBar from "./HeadBar";
 
 interface Props {
   children: React.ReactNode;
@@ -9,8 +10,8 @@ interface Props {
 
 const Layout = ({ children }: Props) => {
   return (
-    <div className="w-screen h-screen relative">
-      <Heading />
+    <div className="w-screen h-full">
+      <HeadBar />
       <main className="relative flex flex-col xl:px-20 lg:px-16 xs:px-5 desktop:py-24 xs:py-12 max-desktop">
         <div className="max-w-[1280px] self-center">{children}</div>
       </main>
