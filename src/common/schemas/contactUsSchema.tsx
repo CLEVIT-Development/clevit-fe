358f987f9ec90f559db1e5107c73f0d2a313c234
hovo-dev <hovhannes.baghdasaryan.03@gmail.com>
2024-05-13 16:53:38 +0400

    fix: fix build action

--- src/common/schemas/contactUsSchema.tsx
+++ src/common/schemas/contactUsSchema.tsx
@@ -4,7 +4,7 @@ export const contactUsSchema = yup.object().shape({
   fullname: yup.string().required("Full Name Required"),
   email: yup.string().email().required("Email Required"),
   country: yup.string().required("Country Required"),
-  phone: yup.string().notRequired(),
+  phone: yup.string(),
   service: yup.string().required("Service Required"),
-  description: yup.string().notRequired(),
+  description: yup.string(),
 });
