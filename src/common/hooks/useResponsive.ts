dee1b597971c2aa1c75b8ccbd43737cfceb6be6b
hovo-dev <hovhannes.baghdasaryan.03@gmail.com>
2024-05-13 19:13:33 +0400

    feat: add contact-us and calendly page fix

--- src/common/hooks/useResponsive.ts
+++ src/common/hooks/useResponsive.ts
@@ -7,6 +7,8 @@ const useResponsive = () => {
     isExtraSmall: width <= 640,
     isMobile: width <= 768,
     isTablet: width <= 1024,
+    // Calendly breakpoint differs from 1024px, so I need configure my styles based on calendly
+    isCalendlyDesktop: width <= 1045,
     isDesktop: width <= 1260,
     isLarge: width <= 1440,
     isExtraLarge: width <= 1600,
--- src/pages/home/CalendlySection.tsx
+++ src/pages/home/CalendlySection.tsx
@@ -1,16 +1,22 @@
 import { InlineWidget } from "react-calendly";
 
 import { appConfig } from "@/common/constants/config.constants.ts";
+import useResponsive from "@/common/hooks/useResponsive.ts";
 import Section from "@/common/templates/Section";
 
 const CalendlySection = () => {
+  const { isTablet, isMobile, isCalendlyDesktop } = useResponsive();
+
   return (
     <Section
       title="Ready to turn your vision into reality? The journey starts here. We're excited to meet you."
-      className="calendlyDesktop:!mb-[-200px] sm:!mb-[-30px] !-mb-[160px]"
+      className="calendlyDesktop:!mb-[50px] space-y-0"
     >
       <div className="w-full">
-        <InlineWidget url={appConfig.shareUrl} styles={{ height: 1100 }} />
+        <InlineWidget
+          url={appConfig.shareUrl}
+          styles={{ height: isMobile ? 1000 : isTablet || isCalendlyDesktop ? 1100 : 700 }}
+        />
       </div>
     </Section>
   );
