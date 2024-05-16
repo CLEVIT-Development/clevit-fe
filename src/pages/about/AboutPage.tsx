3fa69adb7feb5b3a1d4643c932c0e5340be55ea6
hovo-dev <hovhannes.baghdasaryan.03@gmail.com>
2024-05-16 21:47:17 +0400

    fix: fix scroll bug fix

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -18,7 +18,7 @@ const AboutPage = () => {
       <ReasonSection />
       <TotalSection projectsCount={35} stuffCount={10} servedCount={20} />
       <FeedbackSection />
-      <CalendlySection />
+      <CalendlySection title="Ready to take the first step towards achieving your goals? Schedule a free call with a member of our team who will be happy to discuss the services we offer and how they can benefit you." />
     </Layout>
   );
 };
--- src/pages/blog/BlogPage.tsx
+++ src/pages/blog/BlogPage.tsx
@@ -1,13 +1,11 @@
+import { appConfig } from "@/assets/constants/config.constants.ts";
 import Layout from "@/common/layout/Layout.tsx";
 import { HeaderVariant, LayoutVariant } from "@/types/variant.types.ts";
 
 const BlogPage = () => {
   return (
     <Layout headerVariant={HeaderVariant.Tertiary} layoutVariant={LayoutVariant.Secondary}>
-      <iframe
-        src="https://v2-embednotion.com/26f4d4d6c39b45b7a4ba2dc2f0d01af8"
-        className="h-[4600px]"
-      />
+      <iframe src={appConfig.notionBlogUrl} className="h-[2800px] -mb-[450px]" />
     </Layout>
   );
 };
