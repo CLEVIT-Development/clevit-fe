5884ddb952212a7355b837f238080a094f5ac60f
Roman Bernetsian <rbernecyan@gmail.com>
2024-06-10 00:27:46 +0400

    Fix: Watsapp, Viber - icons,  Contact info numbers wrapper padding Feat: Contact us page - Instagram icon-link

--- src/assets/constants/socials.constants.ts
+++ src/assets/constants/socials.constants.ts
@@ -1,7 +1,10 @@
 import Facebook from "@/assets/vectors/Facebook.svg?react";
+import Instagram from "@/assets/vectors/Instagram.svg?react";
 import LinkedIn from "@/assets/vectors/Linkedin.svg?react";
 
 export const socialLinksConstants = [
   { id: 1, link: "https://www.linkedin.com/company/clevit-soft/mycompany/", Icon: LinkedIn },
   { id: 2, link: "https://www.facebook.com/profile.php?id=61557658923832", Icon: Facebook },
+  // TODO: Replace the placeholder link with the actual Instagram profile link
+  { id: 3, link: "https://www.instagram.com/$=profileid", Icon: Instagram },
 ];
--- src/pages/contact-us/ContactWrapper.tsx
+++ src/pages/contact-us/ContactWrapper.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const ContactWrapper = ({ children }: Props) => {
   return (
-    <div className="desktop:min-w-full min-w-[300px] flex  space-y-4 flex-col items-center rounded-lg-l  border border-purple-700 desktop:py-[30px] desktop:pl-[30px] desktop:pr-[40px] py-4 pl-4 pr-9">
+    <div className="desktop:min-w-full min-w-[300px] flex  space-y-4 flex-col rounded-lg-l  border border-purple-700 desktop:py-[30px] desktop:pl-[30px] desktop:pr-[40px] py-4 pl-4 pr-4">
       {children}
     </div>
   );
