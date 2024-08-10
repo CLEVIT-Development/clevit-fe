a0f9ab7e482474ba563f70fd9dc015b37967a226
Hovo-Dev <126023074+Hovo-Dev@users.noreply.github.com>
2024-08-10 21:55:42 +0400

    Fix/development fix (#92)
    * feat: add services pages
    
    * feat: add new technology description
    
    * fix: development sections

--- src/pages/services/sections/ShowcaseSection.tsx
+++ src/pages/services/sections/ShowcaseSection.tsx
@@ -18,7 +18,7 @@ const ShowcaseSection = ({ title, description }: Props) => {
         {title}
       </h4>
       <p className="text-base md:text-md md:text-center !mt-4 lg:!mt-6">{description}</p>
-      <Button className="!mt-12 md:!mt-[60px]" onClick={() => navigate(RoutePaths.Portfolio)}>
+      <Button className="!mt-12 md:!mt-[60px] z-20" onClick={() => navigate(RoutePaths.Portfolio)}>
         See Our Portfolio
       </Button>
       <div className="absolute inset-0 flex items-center justify-start">
