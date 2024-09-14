7f483d63196cf8fd3e1905bda57612bc19c23c1b
Roman Bernetsian <rbernecyan@gmail.com>
2024-09-14 19:10:06 +0400

    Feat: Single blog page Fix: Gap of blog items in blog page, routes

--- src/pages/blog/BlogSection.tsx
+++ src/pages/blog/BlogSection.tsx
@@ -5,15 +5,15 @@ import BlogCard from "@/shared/ui/BlogCard/BlogCard";
 const BlogSection = () => {
   return (
     <Section title="" className="scroll-mt-[150px] md:px-0" headingLevel="h2">
-      <div className="h-full w-full rounded-lg bg-white  grid sm:gap- xs:grid-cols-1 xs:gap-0 sm:grid-cols-2 desktop:grid-cols-3">
-        {blogsConstants.map(({ id, description, image, readingTime, imageAlt, date }) => (
+      <div className="h-full w-full rounded-lg bg-white  grid  xs:grid-cols-1 xs:gap-0 sm:grid-cols-2 desktop:grid-cols-3 !gap-6 ">
+        {blogsConstants.map(({ id, title, image, readingTime, imageAlt, date }) => (
           <BlogCard
             id={id}
             date={date}
             image={image}
             imageAlt={imageAlt}
             key={id}
-            description={description}
+            title={title}
             className="shadow-none"
             readingTime={readingTime}
           />
