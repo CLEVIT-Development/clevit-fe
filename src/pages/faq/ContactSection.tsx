5a103ea03ab6fc8583071a32031627bfdd4378bd
Vache Asatryan <vach.asatryan21@gmail.com>
2024-10-17 13:47:19 +0400

    fixed faq question banner size for ipad Air/Pro

--- src/pages/faq/ContactSection.tsx
+++ src/pages/faq/ContactSection.tsx
@@ -13,14 +13,14 @@ const ContactSection = () => {
   };
 
   return (
-    <section className="p-16 w-screen bg-faqContactGradient desktop:space-y-14 flex flex-col font-bold items-center justify-center">
-      <div className="flex flex-col items-center justify-center text-3xl">
+    <section className="p-16 w-screen bg-faqContactGradient desktop:space-y-14 flex flex-col font-bold md:gap-6 desktop:gap-0 items-center justify-center">
+      <div className="flex flex-col md:gap-6 desktop:gap-1 items-center justify-center text-3xl">
         <p className="text-white desktop:text-3xl text-lg text-center">
           If You Have Any Questions?
         </p>
-        <Pointer className="desktop:hidden md:rotate-120 md:scale-100 xs:rotate-[130deg] xs:scale-[.6]" />
+        <Pointer className="md:rotate-120 md:scale-100 xs:rotate-[130deg] xs:scale-[0.6]" />
       </div>
-      <Button variant={ButtonVariant.Primary} onClick={handleNavigate}>
+      <Button variant={ButtonVariant.Primary} className="desktop:!mt-5" onClick={handleNavigate}>
         Contact Us
       </Button>
     </section>
