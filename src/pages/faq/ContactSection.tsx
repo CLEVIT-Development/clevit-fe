f1621361dfc6241fc26f4b0786e02c682e48b234
Avo <fishenkjianavo@gmail.com>
2024-10-17 13:51:37 +0400

    Update ContactSection.tsx
    Signed-off-by: Avo <fishenkjianavo@gmail.com>

--- src/pages/faq/ContactSection.tsx
+++ src/pages/faq/ContactSection.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-
 import { RoutePaths } from "@/app/routing/routing.constants";
 import Pointer from "@/assets/vectors/Pointer.svg?react";
 import Button from "@/shared/ui/Button";
