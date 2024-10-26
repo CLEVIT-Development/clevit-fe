2108bd5d1ab7fe98f62c01dbeb01cad160137d4a
Roman <rbernecyan@gmail.com>
2024-10-27 00:55:40 +0400

    test

--- api/generateSitemap.ts
+++ api/generateSitemap.ts
@@ -1,34 +1,34 @@
-// import { waitUntil } from "@vercel/functions";
+import { waitUntil } from "@vercel/functions";
 
-// const ServicesIdConstants = {
-//   Web: "web-development",
-//   Mobile: "mobile-app-development",
-//   DevOps: "devops",
-//   AiIntegration: "ai-integration",
-//   MachineLearning: "machine-learning",
-//   QualityAssurance: "quality-assurance",
-//   TechnicalAssessment: "technical-assessment",
-//   MaintenanceSupport: "maintenance-support",
-//   OutStaffing: "outstaffing",
-//   ProjectManagement: "project-management",
-//   Design: "ui-ux-design",
-//   DigitalMarketing: "digital-marketing",
-// };
+const ServicesIdConstants = {
+  Web: "web-development",
+  Mobile: "mobile-app-development",
+  DevOps: "devops",
+  AiIntegration: "ai-integration",
+  MachineLearning: "machine-learning",
+  QualityAssurance: "quality-assurance",
+  TechnicalAssessment: "technical-assessment",
+  MaintenanceSupport: "maintenance-support",
+  OutStaffing: "outstaffing",
+  ProjectManagement: "project-management",
+  Design: "ui-ux-design",
+  DigitalMarketing: "digital-marketing",
+};
 
-// const staticRoutes = [
-//   "/",
-//   "/about-us",
-//   "/services",
-//   "/portfolio",
-//   "/faq",
-//   "/contact-us",
-//   "/privacy-policy",
-//   "/terms-and-conditions",
-//   "/request-demo",
-//   "/blogs",
-// ];
+const staticRoutes = [
+  "/",
+  "/about-us",
+  "/services",
+  "/portfolio",
+  "/faq",
+  "/contact-us",
+  "/privacy-policy",
+  "/terms-and-conditions",
+  "/request-demo",
+  "/blogs",
+];
 
-// const serviceRoutes = Object.values(ServicesIdConstants).map((id) => `/services/${id}`);
+const serviceRoutes = Object.values(ServicesIdConstants).map((id) => `/services/${id}`);
 
 // export default async function generateSitemap(res: any) {
 //   try {
@@ -73,6 +73,43 @@
 //   }
 // }
 
+async function getBlog() {
+  const backendUrl = "https://clevit-be.vercel.app/users/v1/";
+
+  const res = await fetch(`${backendUrl}blogs?page=1&sort=Desc`);
+  return res.json();
+}
+
 export function GET(_request: Request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+  let response: any;
+  waitUntil(
+    getBlog().then((json) => {
+      response = json;
+    })
+  );
+
+  const blogRoutes = response.data.blogsList.map(
+    (blog: { titlePath: string }) => `/blogs/${blog.titlePath}`
+  );
+
+  const allRoutes = [...staticRoutes, ...serviceRoutes, ...blogRoutes];
+
+  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${allRoutes
+    .map(
+      (route) => `
+  <url>
+    <loc>https://www.clevit.io${route}</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.7</priority>
+  </url>
+  `
+    )
+    .join("")}
+    </urlset>`;
+
+  return new Response(sitemapXml, {
+    headers: { "Content-Type": "application/xml" },
+  });
 }
