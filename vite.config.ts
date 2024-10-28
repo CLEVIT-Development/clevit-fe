3c55a4b0788e2992b3a959f834d34b08bf0b75d7
Roman <rbernecyan@gmail.com>
2024-10-29 00:31:14 +0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       app: path.resolve(__dirname, "./src/app"),
